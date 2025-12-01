a2f99a3e32e6dcee96e6083409e11f75655fed59
Tommy <35355900+thomasgrz@users.noreply.github.com>
2025-11-30 22:12:26 -0800

    chore(storybook): init storybook setup (#14)
    Co-authored-by: thomasgrz <thomasgrz@outlok.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslint from "@eslint/js";
 import reactPlugin from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
@@ -6,4 +9,5 @@ export default tseslint.config(
   eslint.configs.recommended,
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat["jsx-runtime"],
+  storybook.configs["flat/recommended"]
 );
