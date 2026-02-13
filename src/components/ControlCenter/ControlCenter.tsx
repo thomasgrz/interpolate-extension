2d6ec473aa16fb8a855b92a8af0d54da959cd5e3
thomasgrz <thomasgrz@outlok.com>
2026-02-12 18:45:41 -0800

    style(script form): fix scripts width, add form coloring

--- src/components/ControlCenter/ControlCenter.tsx
+++ src/components/ControlCenter/ControlCenter.tsx
@@ -1,4 +1,4 @@
-import { Flex, Card, Container } from "@radix-ui/themes";
+import { Flex, Card, Container, Box } from "@radix-ui/themes";
 import { Import } from "../Import/Import.tsx";
 import { Exporter } from "../Exporter/Exporter.tsx";
 import { DashboardNav } from "../DashboardNav/DashboardNav.tsx";
@@ -11,14 +11,28 @@ import { useInterpolateFormSelection } from "../../hooks/useInterpolateFormSelec
 import styles from "./ControlCenter.module.scss";
 import { FormType } from "@/constants";
 import { useInterpolationForm } from "../../hooks/useInterpolationForm/useInterpolationForm.ts";
+import { useMemo } from "react";
 
 export const ControlCenter = () => {
   const { interpolations } = useInterpolations();
   const { selectedForm, setSelectedForm } = useInterpolateFormSelection();
   const form = useInterpolationForm();
+  const formColor = useMemo(() => {
+    switch (selectedForm) {
+      case FormType.HEADER:
+        return "green";
+      case FormType.REDIRECT:
+        return "blue";
+      case FormType.SCRIPT:
+        return "purple";
+      default:
+        return "white";
+    }
+  }, [selectedForm]);
+
   return (
-    <Container className={styles.ControlCenterContainer}>
-      <Flex p="2" className={styles.ImportExportCTAs} justify="between">
+    <Flex gap="1" direction="column" className={styles.ControlCenterContainer}>
+      <Flex pb="0" p="2" className={styles.ImportExportCTAs} justify="between">
         <Import />
         <Exporter
           interpolations={interpolations}
@@ -31,29 +45,35 @@ export const ControlCenter = () => {
         flexGrow={"1"}
         data-testid={"dashboard"}
         justify={"start"}
-        p="3"
+        p="2"
+        pt="0"
+        className={styles.FormContainer}
       >
-        <DashboardNav value={selectedForm} onChange={setSelectedForm} />
-        <Card variant="surface">
-          <Flex height={"100%"} direction="column" flexGrow={"1"}>
-            <form>
-              {selectedForm === FormType.REDIRECT && (
-                // @ts-expect-error id default not defined
-                <RedirectForm form={form} />
-              )}
-              {selectedForm === FormType.HEADER && (
-                // @ts-expect-error id default not defined
-                <HeaderForm form={form} />
-              )}
-              {selectedForm === FormType.SCRIPT && (
-                // @ts-expect-error id default not defined
-                <ScriptForm form={form} />
-              )}
-            </form>
-          </Flex>
-        </Card>
+        <Flex direction={"column"} width={"100%"}>
+          <Box p="2">
+            <DashboardNav value={selectedForm} onChange={setSelectedForm} />
+          </Box>
+          <Card variant="surface" p="0" style={{ backgroundColor: formColor }}>
+            <Flex height={"100%"} direction="column" flexGrow={"1"}>
+              <form>
+                {selectedForm === FormType.REDIRECT && (
+                  // @ts-expect-error id default not defined
+                  <RedirectForm form={form} />
+                )}
+                {selectedForm === FormType.HEADER && (
+                  // @ts-expect-error id default not defined
+                  <HeaderForm form={form} />
+                )}
+                {selectedForm === FormType.SCRIPT && (
+                  // @ts-expect-error id default not defined
+                  <ScriptForm form={form} />
+                )}
+              </form>
+            </Flex>
+          </Card>
+        </Flex>
         <GlobalInterpolationOptions />
       </Flex>
-    </Container>
+    </Flex>
   );
 };
