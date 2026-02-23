5d7f2847f0b6ee565bf26c3ec99d4b599785ca3e
thomasgrz <thomasgrz@outlok.com>
2026-02-22 18:51:27 -0800

    chore: turnoff vid cap

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,6 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    video: "on",
-
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
