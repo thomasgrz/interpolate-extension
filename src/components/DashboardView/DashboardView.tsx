81a4742346db6796edcf5d41b85cf0796e2dada6
Tommy <35355900+thomasgrz@users.noreply.github.com>
2026-03-23 19:15:56 -0700

    style(control center): make dashboard controls stick to top of viewport (#115)
    Co-authored-by: thomasgrz <thomasgrz@outlok.com>

--- src/components/DashboardView/DashboardView.tsx
+++ src/components/DashboardView/DashboardView.tsx
@@ -1,10 +1,13 @@
-import { Callout, Container, Flex } from "@radix-ui/themes";
+import { Callout, Container, Flex, Tabs } from "@radix-ui/themes";
 import { ErrorBoundary } from "react-error-boundary";
 import styles from "./DashboardView.module.scss";
 import { InterpolationsListView } from "../InterpolationsListView/InterpolationsListView.tsx";
 import { ControlCenter } from "../ControlCenter/ControlCenter.tsx";
+import { useInterpolationsContext } from "#src/hooks/useInterpolationsContext/useInterpolationsContext.ts";
 
 export const DashboardView = () => {
+  const { interpolations, recentlyActive } = useInterpolationsContext();
+
   return (
     <ErrorBoundary
       onError={console.error}
@@ -16,16 +19,34 @@ export const DashboardView = () => {
     >
       <Container pb="50px" className={styles.Container} minHeight={"100dvh"}>
         <Flex
-          p="1"
+          p="0"
           minHeight={"100dvh"}
           flexGrow={"1"}
           justify={"start"}
           direction={"column"}
         >
-          <Flex className={styles.FormArea} justify="center">
-            <ControlCenter />
-          </Flex>
-          <InterpolationsListView />
+          <Tabs.Root className={styles.TabsRoot} defaultValue="all">
+            <Flex
+              direction="column"
+              className={styles.DashboardControls}
+              align={"center"}
+              justify="center"
+            >
+              <ControlCenter />
+              <Tabs.List>
+                <Flex className={styles.Tabs} justify={"center"}>
+                  <Tabs.Trigger value="all">All interpolations</Tabs.Trigger>
+                  <Tabs.Trigger value="active">Active in tab</Tabs.Trigger>
+                </Flex>
+              </Tabs.List>
+            </Flex>
+            <Tabs.Content value="all">
+              <InterpolationsListView configs={interpolations} />
+            </Tabs.Content>
+            <Tabs.Content value="active">
+              <InterpolationsListView configs={recentlyActive} />
+            </Tabs.Content>
+          </Tabs.Root>
         </Flex>
       </Container>
     </ErrorBoundary>
--- src/components/InterpolationsListView/InterpolationsListView.tsx
+++ src/components/InterpolationsListView/InterpolationsListView.tsx
@@ -1,53 +1,24 @@
 import { Flex, Box, Tabs } from "@radix-ui/themes";
 import { InterpolationCard } from "../InterpolationCard/InterpolationCard.tsx";
 import styles from "./InterpolationsListView.module.scss";
-import { useInterpolationsContext } from "#src/hooks/useInterpolationsContext/useInterpolationsContext.ts";
+import { AnyInterpolation } from "#src/utils/factories/Interpolation.ts";
 
-export const InterpolationsListView = () => {
-  const { interpolations, recentlyActive } = useInterpolationsContext();
-
+export const InterpolationsListView = ({
+  configs,
+}: {
+  configs?: AnyInterpolation[];
+}) => {
   return (
-    <Flex width="100%" p="1" direction={"row"} wrap="wrap">
-      <Tabs.Root className={styles.TabsRoot} defaultValue="all">
-        <Tabs.List>
-          <Flex className={styles.Tabs} justify={"center"}>
-            <Tabs.Trigger value="all">All interpolations</Tabs.Trigger>
-            <Tabs.Trigger value="active">Active in tab</Tabs.Trigger>
-          </Flex>
-        </Tabs.List>
-
-        <Box pt="3">
-          <Tabs.Content value="all">
-            <Flex direction="column">
-              {interpolations?.map?.((interpolation) => (
-                <Box
-                  key={interpolation.details?.id}
-                  p="1"
-                  className={styles.InterpolationsCardContainer}
-                >
-                  <Flex gap="3">
-                    <InterpolationCard info={interpolation} />
-                  </Flex>
-                </Box>
-              ))}
-            </Flex>
-          </Tabs.Content>
-
-          <Tabs.Content value="active">
-            {recentlyActive?.map?.((interpolation) => (
-              <Box
-                key={interpolation.details?.id + "-active"}
-                p="1"
-                className={styles.InterpolationsCardContainer}
-              >
-                <Flex gap="3">
-                  <InterpolationCard info={interpolation} />
-                </Flex>
-              </Box>
-            ))}
-          </Tabs.Content>
+    <Flex direction="column">
+      {configs?.map?.((interpolation) => (
+        <Box
+          key={interpolation.details?.id}
+          p="1"
+          className={styles.InterpolationsCardContainer}
+        >
+          <InterpolationCard info={interpolation} />
         </Box>
-      </Tabs.Root>
+      ))}
     </Flex>
   );
 };
