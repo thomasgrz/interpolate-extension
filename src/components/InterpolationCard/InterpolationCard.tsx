94d01e6ecbaff750680b19f63b9d02f7ba6f0a0d
thomasgrz <thomasgrz@outlok.com>
2026-04-01 21:36:07 -0700

    chore: rm debug log

--- src/components/InterpolationCard/InterpolationCard.tsx
+++ src/components/InterpolationCard/InterpolationCard.tsx
@@ -195,7 +195,6 @@ export const InterpolationCard = ({
           />
         );
       case "script":
-        console.log({ defaultValues: info.details });
         return (
           <UserScriptForm
             mode="edit"
