8477aef2594a28e3560244e0a15696bde4461429
Tommy <35355900+thomasgrz@users.noreply.github.com>
2026-07-12 21:24:53 -0700

    feat(viewport ui): hide when interpolate is disabled  (#165)
    * feat(browser ui): hide when globally disabled
    
    * chore: minor bump
    
    * chore: make prop optional
    
    ---------
    
    Co-authored-by: thomasgrz <tommygdev@gmail.com>

--- src/components/GlobalInterpolationOptions/GlobalInterpolationOptions.tsx
+++ src/components/GlobalInterpolationOptions/GlobalInterpolationOptions.tsx
@@ -3,11 +3,20 @@ import styles from "./GlobalInterpolationOptions.module.scss";
 import { useInterpolationsContext } from "#src/hooks/useInterpolationsContext/useInterpolationsContext.ts";
 import { ResumeIcon, StopIcon } from "@radix-ui/react-icons";
 
-export const GlobalInterpolationOptions = () => {
+export const GlobalInterpolationOptions = ({
+  onChange,
+}: {
+  onChange?: (_value: boolean) => void;
+}) => {
   const { isExtensionEnabled, disableExtension, enableExtension } =
     useInterpolationsContext();
 
   const isExtensionDisabled = !isExtensionEnabled;
+
+  const handleDisableExtension = () => {
+    disableExtension();
+    onChange?.(false);
+  };
   return isExtensionDisabled ? (
     <Tooltip content="Enable interpolate extension in browser">
       <IconButton
@@ -31,7 +40,7 @@ export const GlobalInterpolationOptions = () => {
         data-testid="pause-all"
         className={styles.PauseAllRules}
         color="red"
-        onClick={disableExtension}
+        onClick={handleDisableExtension}
       >
         <StopIcon />
       </IconButton>
